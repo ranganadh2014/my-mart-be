90435d604cedb29459ad90a6d2734515c50b30a6
Ranganadh Thatha <ranganadh.thatha@gmail.com>
2025-01-01 00:20:17 +0530

    Passport Local Strategy & Guards implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot(), LoggerModule.forRoot()],
+  imports: [UsersModule, ConfigModule.forRoot(), LoggerModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })

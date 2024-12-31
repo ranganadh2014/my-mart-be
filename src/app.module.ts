b1f20cc5fcc9fc7e5fb4745510ff146c1f32f6a2
Ranganadh Thatha <ranganadh.thatha@gmail.com>
2024-12-31 15:53:51 +0530

    user module and db integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserDto){
-    return data;
+  constructor(private readonly prismaService: PrismaService){}
+
+  async createUser(data: CreateUserDto){
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        }
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw error;
+    }
+
   }
 }

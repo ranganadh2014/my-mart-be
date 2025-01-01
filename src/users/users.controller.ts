59aca98f18cdb9c3b6de3d2ac73c3c4c82bca9be
Ranganadh Thatha <ranganadh.thatha@gmail.com>
2025-01-01 15:06:09 +0530

    Added Jwt Strategy and Guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,4 +10,11 @@ export class UsersController {
   createUser(@Body() request: CreateUserDto) {
     return this.usersService.createUser(request);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  getProtected() {
+    return 'You are authorized';
+  }
+
 }

ecbac81b422f4c2515435efd51fec2b506bf3b7f
Ranganadh Thatha <ranganadh.thatha@gmail.com>
2025-01-08 07:42:58 +0530

    Added helmet middleware to prevent vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   app.use(cookieParser());
+  //helmet middleware to prevent wellknown vulnerabilities
+  app.use(helmet());
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
